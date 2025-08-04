1f4bbbd98da3a1b0fa4f3f656e3a9d7abd41b607
Txhx2130 <110928807+Txhx2130@users.noreply.github.com>
2025-08-04 18:11:26 +0200

    Add files via upload

---
+++ Runtime/Scripts/Components/ILckMixer.cs
@@ -0,0 +1,32 @@
+using System;
+using Liv.Lck.Recorder;
+
+namespace Liv.Lck
+{
+    internal interface ILckMixer : IDisposable
+    {
+        LckResult ActivateCameraById(string cameraId, string monitorId = null);
+        void SetPreviewActive(bool isActive);
+        public LckResult<ILckCamera> GetActiveCamera();
+        public LckResult StopActiveCamera();
+        LckResult StartRecording();
+        LckResult StopRecording(LckService.StopReason stopReason);
+        bool IsRecording();
+        bool IsCapturing();
+        LckResult CapturePhoto();
+        LckResult SetMicrophoneCaptureActive(bool isActive);
+        LckResult SetGameAudioMute(bool isMute);
+        LckResult<bool> IsGameAudioMute();
+        float GetMicrophoneOutputLevel();
+        float GetGameOutputLevel();
+        LckResult SetTrackResolution(CameraResolutionDescriptor cameraResolutionDescriptor);
+        LckResult SetTrackFramerate(uint framerate);
+        LckResult<TimeSpan> GetRecordingDuration();
+        LckResult SetTrackBitrate(uint bitrate);
+        LckResult SetTrackDescriptor(CameraTrackDescriptor cameraTrackDescriptor);
+        LckResult SetTrackAudioBitrate(uint audioBitrate);
+        void SetMicrophoneGain(float gain);
+        void SetGameAudioGain(float gain);
+        LckResult<LckDescriptor> GetCurrentTrackDescriptor();
+    }
+}
